fe10e2aeac968edd42e0128a642666c5bce56c49
Daniel <danielmil123@hotmail.com>
2019-06-09 22:20:56 -0400

    Registration functionality

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,7 +5,7 @@ import bcrypt from 'bcrypt';
 // User model config
 const UserSchema: mongoose.Schema<any>  = new Schema({ 
     username: {type: String, required: true, unique: true },
-    password: {type: String, required: true, }
+    password: {type: String, required: true }
  });
 
 UserSchema.plugin(uniqueValidator);
@@ -13,22 +13,18 @@ UserSchema.plugin(uniqueValidator);
 interface userModel {
     username: string,
     password: string,
-    save: () => Promise<any>
+    save: (err: Error) => void
 };
 
 export const User: mongoose.Model<any> = mongoose.model("User", UserSchema);
 
-export function saveUser(newUser: userModel) {    
+export function saveUser(newUser: userModel, callback: any) {    
     bcrypt.genSalt(10, (err: Error, salt: string) => {
         if (err) console.error(err);
         bcrypt.hash(newUser.password, salt, (err: Error, hash: string) => {
             if (err) console.error(err);
             newUser.password = hash;
-            newUser.save()
-            .then(() => console.log('Succesfully created user'))
-            .catch((err) => {
-                throw Error(err)
-            });
+            newUser.save(callback); 
         });
     });
 }
