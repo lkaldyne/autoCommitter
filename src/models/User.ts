6788993b13cfa88665cdb7fc1d3ad154b95f441f
DanielMilGuelph <dmil@mail.uoguelph.ca>
2019-06-09 21:18:47 -0400

    Added registration

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,34 @@
 import mongoose, { Schema } from 'mongoose';
 import uniqueValidator  from 'mongoose-unique-validator';
+import bcrypt from 'bcrypt';
 
 // User model config
 const UserSchema: mongoose.Schema<any>  = new Schema({ 
     username: {type: String, required: true, unique: true },
     password: {type: String, required: true, }
  });
-UserSchema.plugin(uniqueValidator);
-const User: mongoose.Model<any> = mongoose.model("User", UserSchema);
 
-export default User; 
+UserSchema.plugin(uniqueValidator);
+
+interface userModel {
+    username: string,
+    password: string,
+    save: () => Promise<any>
+};
+
+export const User: mongoose.Model<any> = mongoose.model("User", UserSchema);
+
+export function saveUser(newUser: userModel) {    
+    bcrypt.genSalt(10, (err: Error, salt: string) => {
+        if (err) console.error(err);
+        bcrypt.hash(newUser.password, salt, (err: Error, hash: string) => {
+            if (err) console.error(err);
+            newUser.password = hash;
+            newUser.save()
+            .then(() => console.log('Succesfully created user'))
+            .catch((err) => {
+                throw Error(err)
+            });
+        });
+    });
+}
--- src/routes/ProfileRouter.ts
+++ src/routes/ProfileRouter.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-
+import { User, saveUser } from '../models/User'
 
 const router: Router = Router();
 
@@ -11,5 +11,19 @@ router.get('/:id?', (req: Request, res: Response) => {
   const id = req.params.id ? req.params.id : undefined
   // return the profiles
 })
+
+router.post('/register', (req: Request, res: Response) => {
+  let { username, password } = req.body;
+  if (!username || !password) {
+    res.sendStatus(400).send('Bad Request. Missing a required field.');
+  }
+  let newUser = new User({username, password});
+  try {
+    saveUser(newUser);
+  } catch (err) {
+    res.sendStatus(500).send(err);
+  }
+  res.send('Successfully created new user'); 
+});
 
 export const profileRouter: Router = router
