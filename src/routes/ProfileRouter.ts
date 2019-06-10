4930f3b299ff34aa0fd95cf6226b2ad1be95fea7
Daniel <danielmil123@hotmail.com>
2019-06-09 22:20:56 -0400

    Registration functionality

--- src/routes/ProfileRouter.ts
+++ src/routes/ProfileRouter.ts
@@ -12,18 +12,14 @@ router.get('/:id?', (req: Request, res: Response) => {
   // return the profiles
 })
 
-router.post('/register', (req: Request, res: Response) => {
+router.post('/register', async (req: Request, res: Response) => {
   let { username, password } = req.body;
-  if (!username || !password) {
-    res.sendStatus(400).send('Bad Request. Missing a required field.');
-  }
+  if (!username || !password) res.status(400).send({error: 'Bad Request. Missing a required field.'});
   let newUser = new User({username, password});
-  try {
-    saveUser(newUser);
-  } catch (err) {
-    res.sendStatus(500).send(err);
-  }
-  res.send('Successfully created new user'); 
+  saveUser(newUser, (err: Error) => {
+    if (err) res.status(500).send({ err });
+    else res.send('Successfully created new user');  
+  });
 });
 
 export const profileRouter: Router = router
