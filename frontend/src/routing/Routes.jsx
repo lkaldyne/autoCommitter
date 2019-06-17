d595001441431cf16611cda080acc0e1788bc814
lkaldyne <l.kaldyne@gmail.com>
2019-06-17 12:39:42 -0400

    Routes first commit

---
+++ frontend/src/routing/Routes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter, Route, Redirect} from 'react-router-dom'
+import { HomePage } from '../pages/HomePage';
+import { Dashboard } from '../pages/Dashboard';
+
+export class Routes extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Route exact path="/" render={() => (
+          <HomePage />           
+        )}/>
+        <Route exact path="/dashboard" render={() => (
+          <Dashboard />
+        )}/>
+      </BrowserRouter>
+    )
+  }
+}
