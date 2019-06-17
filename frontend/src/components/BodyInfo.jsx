baffd3d296e6d85cfd7e556f25cf9f22de83466f
lkaldyne <l.kaldyne@gmail.com>
2019-06-17 18:07:48 -0400

    Description and styling changes

--- frontend/src/components/BodyInfo.jsx
+++ frontend/src/components/BodyInfo.jsx
@@ -4,8 +4,10 @@ export default class BodyInfo extends React.Component {
   render() {
     return (
         <div style={wrapperStyle}>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+            <h1>Welcome to AutoCommiter.</h1>
+            <p style={pStyle}>If you want your Github commit history to look as beautiful as the image below, but you're way too lazy to put that level of commitment (pun intended), then look no further.<br /><br />AutoCommitter saves you the hassle and commits on your behalf. Just make an account with your github token and you're all set!</p>
             <img src="\commits.png" alt="GitHub Commit Contributions" style={imageStyle}/>
+            <p>AutoCommitter is modular. Once your account is made, you can customize how frequently and how randomly your commits will occur, as well as other settings.</p>
         </div>
     )
   }
@@ -14,8 +16,9 @@ export default class BodyInfo extends React.Component {
 const wrapperStyle = {
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'column'
+    alignItems: 'left',
+    flexDirection: 'column',
+    paddingRight: '120px'
 }
 
 const imageStyle = {
@@ -23,4 +26,8 @@ const imageStyle = {
     width: '70%',
     height: 'auto',
     boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
+}
+
+const pStyle = {
+  fontSize: '20px'
 }
