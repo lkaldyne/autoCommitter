6b7c3fca3cfeffa4886a2a4bb0eb3316ccb6381a
lkaldyne <l.kaldyne@gmail.com>
2019-06-18 12:10:54 -0400

    Added User info Display and Logout functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,63 @@
 import React from 'react'
 import LoginForm from '../components/Forms/LoginForm';
 import PropTypes from 'prop-types';
+import { Popover, PopoverHeader, PopoverBody } from 'reactstrap';
 
-export default function Header(props) {
-    return props.isHomepage ? (
-        <div style={headerDiv}>
+export default class Header extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoginHovered: false,
+            popoverOpen: false
+        }
+    }
+
+    toggle = () => {
+        this.setState(prevState => ({
+            popoverOpen: !prevState.popoverOpen
+        }));
+    }
+
+    render() {
+
+        let titleElement = (
             <div style={{margin: '15px'}}>
-                <a href='/' style={{textDecoration: 'none'}}><h1 style={title}>{props.title}</h1></a>
+                <a href='/' style={{textDecoration: 'none'}}><h1 style={title}>{this.props.title}</h1></a>
             </div>
-            <div style={{margin: '15px'}}>
-                <LoginForm />
+        )
+
+        return this.props.isHomepage ? (
+            <div style={headerDiv}>
+                {titleElement}
+                <div style={{margin: '15px'}}>
+                    <LoginForm />
+                </div>
             </div>
-        </div>
-    ) : (
-        <div style={headerDiv}>
-            <div style={{margin: '15px'}}>
-                <a href='/' style={{textDecoration: 'none'}}><h1 style={title}>{props.title}</h1></a>
+        ) : (
+            <div style={headerDiv}>
+                {titleElement}
+                <div id="LogoutPopover"
+                    style={userInfoHead}
+                    onMouseEnter={() => { 
+                        this.setState({ isLoginHovered: true});
+                        this.setState({popoverOpen: true});
+                    }}
+                    onMouseLeave={() => { 
+                        this.setState({ isLoginHovered: false});
+                    }}
+                >
+                    <img style={userIconStyle} src='\userIcon.png' />
+                    <p style={{margin: '5px 10px 0 15px'}} className={`text-${this.state.isLoginHovered ? 'success' : null}`}>{this.props.userEmail}</p>
+                    <Popover placement="bottom" isOpen={this.state.popoverOpen} target="LogoutPopover" toggle={this.toggle}>
+                        <PopoverHeader>What would you like to do?</PopoverHeader>
+                        <PopoverBody>
+                            <a href='' onClick={this.props.userLogout}>LogOut</a>
+                        </PopoverBody>
+                    </Popover>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }   
 }
 
 const headerDiv = {
@@ -26,7 +65,8 @@ const headerDiv = {
     justifyContent: 'space-between',
     alignItems: 'center',
     height: '5rem',
-    backgroundColor: '#2C3E50'
+    backgroundColor: '#2C3E50',
+    transition: '1s'
 }
 
 const title = {
@@ -38,4 +78,16 @@ const title = {
 
 Header.propTypes = {
     title: PropTypes.string.isRequired,
+}
+
+const userIconStyle = {
+    width: '40px',
+    height: '40px'
+}
+
+const userInfoHead = {
+    color: 'white',
+    margin: '15px',
+    fontSize: '20px',
+    display: 'flex'
 }
