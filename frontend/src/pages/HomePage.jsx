d2f0024c468223bc2a40bee8035f5fffebce30ed
Laith K <l.kaldyne@gmail.com>
2019-06-18 19:09:50 -0400

    Commit Fix

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,34 +2,35 @@ import React from 'react'
 import { PageContainer } from '../components/PageContainer';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { BrowserRouter, Route, Redirect} from 'react-router-dom'
+import { Redirect} from 'react-router-dom'
+import axios from 'axios';
 
 export class HomePage extends React.Component {
-  loggedIn = () => {
-    // check session/application data
-    return true;
+
+  state = {
+    loggedIn: false
+  }
+
+  componentDidMount = () => {
+    axios.defaults.withCredentials = true; 
+    axios('http://localhost:5000/api/profiles/user', { 
+        method: 'get'
+      })
+      .then((response) => this.setState({ loggedIn: true }))
+      .catch((err) => this.setState({loggedIn: false}))
   }
 
   render() {
     return (
+      this.state.loggedIn ? <Redirect push to="/dashboard" /> 
+      : 
+      (
         <React.Fragment>
-          <BrowserRouter>
-            <Header title='AutoCommitter' />
-            <Route exact path="/" render={() => (
-                  <PageContainer />
-            )}/>
-            <Route exact path="/dashboard" render={() => (
-              this.loggedIn ? (
-                <React.Fragment>
-                </React.Fragment>
-              ) : (
-                <Redirect push to="/" />
-              )
-            )}/>
-            <Footer />
-          </BrowserRouter>
+          <Header title='AutoCommitter' isHomepage={true}/>
+          <PageContainer />
+          <Footer />
         </React.Fragment>
-
+      )
     )
   }
 }
