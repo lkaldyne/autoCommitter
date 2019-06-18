94bacc57c06fde29e6eb7fffc8e85c88b9a798fa
DanielMil <dmil@mail.uoguelph.ca>
2019-06-17 22:35:05 -0400

    Redirecting to dashboard when logged in

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,15 +2,35 @@ import React from 'react'
 import { PageContainer } from '../components/PageContainer';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { Redirect} from 'react-router-dom'
+import axios from 'axios';
 
 export class HomePage extends React.Component {
+
+  state = {
+    loggedIn: false
+  }
+
+  componentDidMount = () => {
+    axios.defaults.withCredentials = true; 
+    axios('http://localhost:5000/api/profiles/user', { 
+        method: 'get'
+      })
+      .then((response) => this.setState({ loggedIn: true }))
+      .catch((err) => this.setState({loggedIn: false}))
+  }
+
   render() {
     return (
-      <React.Fragment>
-        <Header title='AutoCommitter' isHomepage={true}/>
-        <PageContainer />
-        <Footer />
-      </React.Fragment>
+      this.state.loggedIn ? <Redirect to="/dashboard" /> 
+      : 
+      (
+        <React.Fragment>
+          <Header title='AutoCommitter' isHomepage={true}/>
+          <PageContainer />
+          <Footer />
+        </React.Fragment>
+      )
     )
   }
 }
