65e7b9974ef735418ec844256ecee68b8618d9dd
Laith K <l.kaldyne@gmail.com>
2019-06-18 19:16:27 -0400

    GitTools First Commit

---
+++ src/utils/GitTools.ts
@@ -0,0 +1,23 @@
+import Account from "./Account";
+
+export default class GitTools {
+    public static commitOneUser(newAccount: Account, callback: () => void): void {
+        newAccount.clone(() => {
+            newAccount.alterFile(() => {
+                newAccount.stage(() => {
+                    newAccount.commit(() => {
+                        console.log("committed");
+                        newAccount.push(() => {
+                            console.log("pushed");
+                            newAccount.removeRepo(() => {
+                                console.log("removed repo");
+                                callback();
+                            })
+                        }, callback);
+                    }, callback);
+                }, callback);
+            });    
+        }, callback);
+
+    }
+}
