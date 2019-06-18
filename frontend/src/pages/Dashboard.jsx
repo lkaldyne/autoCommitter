7c2e41a6c4550ae8b2d48c8b7c8229c5757888da
Laith K <l.kaldyne@gmail.com>
2019-06-18 19:04:53 -0400

    Added button and spinner imports

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { Redirect} from 'react-router-dom'
+import { Redirect} from 'react-router-dom';
+import { Button, Spinner } from 'reactstrap';
 import axios from 'axios';
 
 export class Dashboard extends React.Component {   
