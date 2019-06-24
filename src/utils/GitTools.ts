c7e67a26ec514df3642ec10453a2c360a79c75c7
lkaldyne <l.kaldyne@gmail.com>
2019-06-24 17:15:57 -0400

    pre-existing repo fix

--- src/utils/GitTools.ts
+++ src/utils/GitTools.ts
@@ -3,7 +3,13 @@ import { response } from 'express';
 
 export default class GitTools {
   public static async commitOneUser(account: Account) {
-    await account.clone()
+    try {
+      await account.clone()
+    } catch(e) {
+      await account.removeRepo()
+      await account.clone()
+    }
+    
     await account.alterFile()
     await account.stage()
     await account.commit()
@@ -18,14 +24,10 @@ export default class GitTools {
     account.log(`Number of commits: ${numCommits} / ${account.getMaxNumberOfCommitsPerDay()}`)
     if (isCommitting) {
       try {
-        console.log("trying to Clone")
         await account.clone()
       } catch(e) {
-        console.log("Made it to Clone catch")
         await account.removeRepo()
-        console.log("Removed Repo before cloning")
         await account.clone()
-        console.log("Cloned")
       }
       for (let i = 0; i < numCommits; i++) {
         await account.alterFile()
